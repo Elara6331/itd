5e34f656b328b30d463de0f39ada262b3e7de312
Arsen Musayelyan <moussaelianarsen@gmail.com>
2021-08-21 19:03:18 -0700

    Disable completion command

--- cmd/itctl/cmd/root.go
+++ cmd/itctl/cmd/root.go
@@ -60,5 +60,6 @@ var rootCmd = &cobra.Command{
 // Execute adds all child commands to the root command and sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
+	rootCmd.CompletionOptions.DisableDefaultCmd = true
 	cobra.CheckErr(rootCmd.Execute())
 }
