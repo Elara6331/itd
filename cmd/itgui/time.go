c881502b44f645e61fe87abe094b9c7af50e729a
Elara Musayelyan <elara@elara.ws>
2021-10-24 13:27:14 -0700

    Use api package in itgui

--- cmd/itgui/time.go
+++ cmd/itgui/time.go
@@ -1,18 +1,16 @@
 package main
 
 import (
-	"encoding/json"
-	"net"
 	"time"
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/layout"
 	"fyne.io/fyne/v2/widget"
-	"go.arsenm.dev/itd/internal/types"
+	"go.arsenm.dev/itd/api"
 )
 
-func timeTab(parent fyne.Window) *fyne.Container {
+func timeTab(parent fyne.Window, client *api.Client) *fyne.Container {
 	// Create new entry for time string
 	timeEntry := widget.NewEntry()
 	// Set text to current time formatter properly
@@ -21,7 +19,7 @@ func timeTab(parent fyne.Window) *fyne.Container {
 	// Create button to set current time
 	currentBtn := widget.NewButton("Set Current", func() {
 		timeEntry.SetText(time.Now().Format(time.RFC1123))
-		setTime(true)
+		setTime(client, true)
 	})
 
 	// Create button to set time inside entry
@@ -33,7 +31,7 @@ func timeTab(parent fyne.Window) *fyne.Container {
 			return
 		}
 		// Set time to parsed time
-		setTime(false, parsedTime)
+		setTime(client, false, parsedTime)
 	})
 
 	// Return new container with all elements centered
@@ -48,30 +46,13 @@ func timeTab(parent fyne.Window) *fyne.Container {
 
 // setTime sets the first element in the variadic parameter
 // if current is false, otherwise, it sets the current time.
-func setTime(current bool, t ...time.Time) error {
-	// Dial UNIX socket
-	conn, err := net.Dial("unix", SockPath)
-	if err != nil {
-		return err
-	}
-	defer conn.Close()
-	var data string
-	// If current is true, use the string "now"
-	// otherwise, use the formatted time from the
-	// first element in the variadic parameter.
-	// "now" is more accurate than formatting
-	// current time as only seconds are preserved
-	// in that case.
+func setTime(client *api.Client, current bool, t ...time.Time) error {
+	var err error
 	if current {
-		data = "now"
+		err = client.SetTimeNow()
 	} else {
-		data = t[0].Format(time.RFC3339)
+		err = client.SetTime(t[0])
 	}
-	// Encode SetTime request with above data
-	err = json.NewEncoder(conn).Encode(types.Request{
-		Type: types.ReqTypeSetTime,
-		Data: data,
-	})
 	if err != nil {
 		return err
 	}
