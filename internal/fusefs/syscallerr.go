5ce83902a4a45537d06fa4b070f99a77d9c2fe69
Elara Musayelyan <elara@elara.ws>
2023-03-26 13:01:25 -0700

    Use EINVAL for Invalid Offset error in FUSE

--- internal/fusefs/syscallerr.go
+++ internal/fusefs/syscallerr.go
@@ -58,7 +58,7 @@ func syscallErr(err error) syscall.Errno {
 		case blefs.ErrFileWriteOnly: // file is write only
 			return syscall.EACCES
 		case blefs.ErrInvalidOffset: // invalid file offset
-			return syscall.EFAULT // TODO
+			return syscall.EINVAL
 		case blefs.ErrOffsetChanged: // offset has already been changed
 			return syscall.ESPIPE
 		case blefs.ErrReadOpen: // only one file can be opened for reading at a time
